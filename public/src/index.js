b2d0d53209341d691b32db718361b45aab1eeb1b
ebookcode123 <ebookcode123@gmail.com>
2017-06-01 14:30:10 -0700

    怎么用refs 来代替多个listener 来设计。

--- public/src/index.js
+++ public/src/index.js
@@ -8,22 +8,47 @@ class FormApp extends Component {
 			inputVaule: 'input value',
 			selectValue: 'A',
 			radioValue:'B',
+			checkValues:[],
 			textareaValue: 'some tex here,......'
 		}
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleRadio = this.handleRadio.bind(this);
+		this.handleCheck = this.handleCheck.bind(this);
+
 	}
 
 	handleSubmit(e) {
 		e.preventDefault();
 		var formDate = {
-			input: this.ref.goodInput.value;
-			select: this.ref.goodSelect.value
-			textarea: this.ref.goodTextarea.value
+			input: this.refs.goodInput.value,
+			select: this.refs.goodSelect.value,
+			redio:this.state.radioValue,
+			check: this.state.checkValues,
+			textarea: this.refs.goodTextarea.value
 		}
 
 		console.log('the form result is:')
 		console.log(formDate);
 	}
+
+	handleRadio(e){
+		this.setState({radioValue: e.target.value});
+	};
+
+	handleCheck(e){
+			var checkValues = this.state.checkValues.slice();
+			var newValue = e.target.value;
+			console.log(e.target.value);
+			var index = checkValues.indexOf(newValue);
+			if(index == -1) {
+				checkValues.push(newValue);
+			} else{
+				checkValues.splice(index,1);
+			}
+			this.setState({
+				checkValues:checkValues,
+			})
+	};
 
 	render() {
 		return(
@@ -40,17 +65,18 @@ class FormApp extends Component {
 				<br />
 
 				<p>radio button</p>
-				<input onChange ={handleRadio} name="goodRadio" type="radio" value="A" />
-				<input onChange ={handleRadio} name="goodRadio" type="radio" defaultChecked vaule="B"/>
-				<input onChange ={handleRadio} name="goodRadio" type="radio" vaule="C"/>
+				<p>A B C</p>
+				<input onChange ={this.handleRadio} name="goodRadio" type="radio" value="A" />
+				<input onChange ={this.handleRadio} name="goodRadio" type="radio" defaultChecked vaule="B"/>
+				<input onChange ={this.handleRadio} name="goodRadio" type="radio" vaule="C"/>
 				<br />
 				<p>this is checkbox</p>
-				<input onChange = {handleCheck} name="goodRadio" type="checkbox" vaule="A"/>
-				<input onChange = {handleCheck} name="goodRadio" type="checkbox" vaule="B"/>
-				<input onChange = {handleCheck} name="goodRadio" type="checkbox" defaultChecked vaule="C"/>
+				<input onChange = {this.handleCheck} name="goodRadio" type="checkbox" value="A"/>
+				<input onChange = {this.handleCheck} name="goodRadio" type="checkbox" value="B"/>
+				<input onChange = {this.handleCheck} name="goodRadio" type="checkbox" value="C"/>
 
 				<br/>
-				<textarea defualtVonChange ={handleRadio}alue={this.state.textareaValue} ref = "goodTextarea"></textarea>
+				<textarea defualtValue={this.state.textareaValue} ref = "goodTextarea"></textarea>
 				<button type="submit">submit</button>
 			</form>
 		)

86b3840267bd6aa82c4db831708e4c0b4d0d442a
abc <ebookcode123@gmail.com>
2017-06-10 13:27:27 -0700

    add new style

---
+++ public/src/Question-app没有拆分成模块.js
@@ -0,0 +1,6 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import QuestionApp from './components/QuestionApp';
+
+
+ReactDOM.render(<QuestionApp />, document.getElementById('app'),console.log("finish"));
--- public/src/index.js
+++ public/src/index.js
@@ -1,6 +1,26 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import QuestionApp from './components/QuestionApp';
+
+class HelloMessage extends Component {
 
 
-ReactDOM.render(<QuestionApp />, document.getElementById('app'),console.log("finish"));
+
+  render() {
+    const divStyle= {
+      color: 'red',
+      background:'yellow'
+    }
+    const pstyle = {
+      color: 'blue',
+      background: 'red'
+    }
+    return(
+      <div style={divStyle}>
+        Hello {this.props.name}
+        <p >you are sb</p>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<HelloMessage name ={"lei"} />, document.getElementById('app'));
